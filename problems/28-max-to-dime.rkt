b984d9f7ff54b14252557b19de2e852bf9d8705e
U-DESKTOP-7LBL5AI\siprad <will422a@gmail.com>
2019-08-12 20:38:45 -0400

    27 to 29

---
+++ problems/28-max-to-dime.rkt
@@ -0,0 +1,23 @@
+(define base-price 5.0)
+(define base-attendance 120)
+(define attendance-delta 15)
+(define price-delta 0.1)
+(define performance-cost 180)
+(define cost-per-attendee 0.04)
+
+(define (attendees ticket-price)
+  (- base-attendance (* (- ticket-price base-price) (/ attendance-delta price-delta))))
+
+(define (revenue ticket-price)
+  (* ticket-price (attendees ticket-price)))
+
+(define (cost ticket-price)
+  (+ performance-cost (* cost-per-attendee (attendees ticket-price))))
+
+(define (profit ticket-price)
+  (- (revenue ticket-price)
+     (cost ticket-price)))
+
+(profit 2.8)
+(profit 2.9)
+(profit 3.0)
